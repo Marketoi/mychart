90a7208956a693821dacf9cedd06c8b2bee03987
Germain <germain.blanchet@gmail.com>
2018-11-22 22:18:13 +0700

    Delete tag_latest_lat2_.sh

--- tag_latest_lat2_.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-MANIFEST=$(aws ecr batch-get-image --repository-name mo/ci --image-ids imageTag=latest --query images[].imageManifest --output text)
-aws ecr put-image --repository-name mo/ci --image-tag lat2_ --image-manifest "$MANIFEST"
